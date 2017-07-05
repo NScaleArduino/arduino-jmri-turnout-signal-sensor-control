f0af510964faed38a20d8796fa3c59371e9f7506
William <gcphost@users.noreply.github.com>
2017-07-05 08:47:27 -0700

    Update dpdtversion.ino

--- dpdtversion.ino
+++ dpdtversion.ino
@@ -1,15 +1,5 @@
-
 #include <SPI.h>
-
-
-// http://thenscaler.com/?page_id=661
-// http://thenscaler.com/?page_id=174
-/// https://github.com/madleech/ArduinoCMRI
-// http://jmri.org/community/clinics/NMRA2008/LayoutEditorClinic2008/LayoutEditorClinic.pdf
-
 #include <VarSpeedServo.h>
-
-const int ANALOG_GAP = 40;
 
 const int ALIGN_MAIN = 80;
 const int ALIGN_DIVERGENT = 115;
@@ -18,11 +8,10 @@ const int STEP_DELAY = 10;
 const int NUM_TURNOUTS = 16;
 const int NUM_TOGGLES = 34;
 
+// Set to HIGH and watch console, after entering something into it, for debugging.
+const int DEBUG=LOW;
 
 VarSpeedServo servos[NUM_TURNOUTS];
-
-bool CONNECTED = false;
-
 
 typedef struct TURNOUT_DEF {
   int pin;
@@ -38,13 +27,11 @@ typedef struct TURNOUT_DATA {
   int target_pos;
 };
 
-
 typedef struct TOGGLES {
   int pin;
   char io;
   bool on;
 };
-
 
 TOGGLES toggles[NUM_TOGGLES] = {
   {21, A0, LOW},
@@ -54,7 +41,6 @@ TOGGLES toggles[NUM_TOGGLES] = {
   {25, A5, LOW},
   {26, A6, LOW},
   {27, A7, LOW},
-  
   {28, A8, LOW},
   {29, A9, LOW},
   {30, A10, LOW},
@@ -65,8 +51,6 @@ TOGGLES toggles[NUM_TOGGLES] = {
   {35, A15, LOW},
 };
 
-
-
 TURNOUT_DATA turnouts[NUM_TURNOUTS] = {
   {{2, A0, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
   {{3, A1, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
@@ -75,19 +59,16 @@ TURNOUT_DATA turnouts[NUM_TURNOUTS] = {
   {{6, A4, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
   {{7, A5, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
   {{8, A6, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
-  {{9, A7, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN}, 
-  {{10, A8, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{11, A9, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{12, A10, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{13, A11, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{14, A12, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{15, A13, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{16, A14, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  {{17, A15, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},  
-  };
-
-
-
+  {{9, A7, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{10, A8, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{11, A9, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{12, A10, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{13, A11, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{14, A12, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{15, A13, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{16, A14, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+  {{17, A15, ALIGN_MAIN, ALIGN_DIVERGENT, ALIGN_MAIN}, HIGH, ALIGN_MAIN},
+};
 
 void setup()
 {
@@ -101,55 +82,47 @@ void setup()
 
   for (int i = 0; i < NUM_TOGGLES; i++) {
       pinMode(toggles[i].pin, OUTPUT);
-
       setLed(i, toggles[i].on, true);
   }
-  
 }
 
-char c;
 void loop()
 {
-    CONNECTED = true;
-    
     for (int i = 0; i < NUM_TURNOUTS; i++) {
       setTurnout(i, getBit(turnouts[i].data.io), false);
     }
-  
+
     for (int i = 0; i < NUM_TOGGLES; i++) {
       setLed(i, getBit(toggles[i].io), false);
     }
 }
 
 int getBit(char io) {
- // Serial.println(digitalRead(io));
+  if (DEBUG) {
+    Serial.println(digitalRead(io));
+  }
+
   return analogRead(io) == 0 ? HIGH : LOW;
 }
 
 
 
 void setLed(int id, int status, bool force){
-
-  if (!CONNECTED) {
-    status = toggles[id].on;
-  }
-
   if (toggles[id].on == status && force == false) {
     return true;
   }
-  
+
   toggles[id].on =  status;
-    
+
   digitalWrite(toggles[id].pin, status);
 }
 
 void setTurnout(int id, byte align, bool force) {
-    Serial.println(id); Serial.println(align);
-    
-  if (!CONNECTED) {
-    align = turnouts[id].data.pos_default;
+  if (DEBUG) {
+    Serial.println(id);
+    Serial.println(align);
   }
-  
+
   if (turnouts[id].alignment == align && force == false) {
     return true;
   }
